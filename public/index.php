515e30ff489993f7da9e3a82035383d2e158cb84
neox56 <neox56@gmail.com>
2023-11-12 18:31:36 +0400

    finish step 2

--- public/index.php
+++ public/index.php
@@ -3,12 +3,24 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use DI\Container;
+use Slim\Views\PhpRenderer;
 
 require __DIR__ . '/../vendor/autoload.php';
-$app = AppFactory::create();
-$app->get('/', function (Request $request, Response $response, $args) {
 
-    $response->getBody()->write("Hello world!");
-    return $response;
+$app = AppFactory::create();
+
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $request, Response $response) {
+
+    return $this->get('renderer')->render($response, 'index.phtml');
 });
 $app->run();
